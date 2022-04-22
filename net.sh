36d46a519e52773ccb9703c4260f2ff788ceca15
Kisung Yi <kisungyi92@gmail.com>
2022-04-22 14:29:56 +0000

    net script

---
+++ net.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Creates a backup
+((EUID == 0)) || { echo 'Run as root' >&2; exit 1; }
+
+cp /etc/netplan/01-netcfg.yaml /etc/netplan/01-netcfg.yaml.bk_`date +%Y%m%d%H%M`
+# Changes dhcp from 'yes' to 'no'
+sed -i "s/dhcp4: yes/dhcp4: no/g" /etc/netplan/01-netcfg.yaml
+# Retrieves the NIC information
+nic=`ifconfig | awk 'NR==1{print $1}'`
+# Ask for input on network configuration
+read -p "Enter the static IP of the server in CIDR notation: " staticip 
+read -p "Enter the IP of your gateway: " gatewayip
+read -p "Enter the IP of preferred nameservers (seperated by a coma if more than one): " nameserversip
+echo
+cat > /etc/netplan/01-netcfg.yaml <<EOF
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    $nic
+      addresses:
+        - $staticip
+      gateway4: $gatewayip
+      nameservers:
+          addresses: [$nameserversip]
+EOF
+sudo netplan apply
+echo "==========================="
+echo
